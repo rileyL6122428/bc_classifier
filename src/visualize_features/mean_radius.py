8e4f15d661fd6f4f7d79589b892e303f88300a78
Riley Littlefield <rileylittlefield@ymail.com>
2019-02-13 20:56:13 -0500

    temporarily comments out unnused stats

--- src/visualize_features/mean_radius.py
+++ src/visualize_features/mean_radius.py
@@ -4,22 +4,22 @@ import numpy as np
 from pandas import Series
 from visualize_utils.draw_dispersion import draw_dispersion
 
-mean_radii = X.mean_radius
+# mean_radii = X.mean_radius
 
-print('\n', 'MEAN RADIUS STATS', '\n')
+# print('\n', 'MEAN RADIUS STATS', '\n')
 
-print('MIN MEAN RADIUS =', mean_radii.min())
-print('(MEAN - 1 STD DEV) MEAN RADIUS =', mean_radii.mean() - mean_radii.std())
-print('MEAN MEAN RADIUS =', mean_radii.mean())
-print('(MEAN + 1 STD DEV) MEAN RADIUS =', mean_radii.mean() + mean_radii.std())
-print('MAX MEAN RADIUS =', mean_radii.max(), '\n')
+# print('MIN MEAN RADIUS =', mean_radii.min())
+# print('(MEAN - 1 STD DEV) MEAN RADIUS =', mean_radii.mean() - mean_radii.std())
+# print('MEAN MEAN RADIUS =', mean_radii.mean())
+# print('(MEAN + 1 STD DEV) MEAN RADIUS =', mean_radii.mean() + mean_radii.std())
+# print('MAX MEAN RADIUS =', mean_radii.max(), '\n')
 
-print('MIN MEAN RADIUS =', mean_radii.min())
-print('15% QUANTILE MEAN RADIUS =', mean_radii.quantile(.15))
-print('MEDIAN MEAN RADIUS =', mean_radii.median())
-print('85% QUANTILE MEAN RADIUS =', mean_radii.quantile(.85))
-print('MAX MEAN RADIUS =', mean_radii.max())
-
+# print('MIN MEAN RADIUS =', mean_radii.min())
+# print('15% QUANTILE MEAN RADIUS =', mean_radii.quantile(.15))
+# print('MEDIAN MEAN RADIUS =', mean_radii.median())
+# print('85% QUANTILE MEAN RADIUS =', mean_radii.quantile(.85))
+# print('MAX MEAN RADIUS =', mean_radii.max())
+import pdb; pdb.set_trace()
 draw_dispersion(
     affirmative_series=benign.mean_radius,
     affirmative_label='Benign',
